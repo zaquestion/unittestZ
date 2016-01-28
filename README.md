3d4a4ca5c34534855b6718a9b20dc9806b8064d1
Zaq? Wiedmann <zaqthefreshman@gmail.com>
2016-01-27 19:45:27 -0800

    skeleton of a pypi project

---
+++ README.md
@@ -0,0 +1,7 @@
+unittestZ
+--
+
+Idea
+--
+Dream is for this to be a drop in replacement for the unittest2 assertion framework and maybe one day the test runner itself. It will be written to be compatible with the nosetests (snake case) style assertions and have the alternate function names injected into the namespace.
+
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'unittestZ',
+  packages = ['unittestZ'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Drop in replacement for unittest2/nose test assertion library',
+  author = 'Zaq? Wiedmann',
+  author_email = 'zaquestion@gmail.com',
+  url = 'https://github.com/zaquestion/unittestZ', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'assert', 'assertion', 'unittest2', 'nose'], # arbitrary keywords
+  classifiers = [],
+)
+`
