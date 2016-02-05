9c1bdc750d7c3c25351f0e5673556322b864e933
Zaq? Wiedmann <zaqthefreshman@gmail.com>
2016-02-04 20:18:39 -0800

    Exceptions on failure, more assertions
    Capture stderr when raising AssertionError to surpress output
    Print correct portion of stack when showing details on failed assertion
    
    Added
    - assert_raises
    - assert_false
    - assert_equals

--- unittestZ/assertion.py
+++ unittestZ/assertion.py
@@ -1,33 +1,60 @@
 import sys
 import traceback
+from cStringIO import StringIO
 
 def show_failure():
     s = traceback.extract_stack()
-    f = s[-3]
+    f = {}
+    for e in s[::-1]:
+        if 'assert' in e[3]:
+            f = e
     print '%s:L%s %s' % (f[0], f[1], f[3])
+
+def fail(msg=None):
+    _stderr = sys.stderr
+    sys.stderr = _stringio = StringIO()
+    raise AssertionError(msg)
 
 def assert_equal(actual, expected, msg=None):
     if actual != expected:
         show_failure()
         print "Actual: %s" % actual
         print "Expect: %s" % expected
-        if msg:
-            print msg
+        fail(msg)
 
 def assert_not_equal(actual, expected, msg=None):
     if actual == expected:
         show_failure()
         print "Actual: %s" % actual
         print "Expect: %s" % expected
-        if msg:
-            print msg
+        fail(msg)
 
 def assert_true(expr, msg=None):
     if not expr:
         show_failure()
         print "False"
-        if msg:
-            print msg
+        fail(msg)
+
+def assert_false(expr, msg=None):
+    if expr:
+        show_failure()
+        print "False"
+        fail(msg)
+
+def assert_raises(exception, func, *args, **kwds):
+    _stdout = sys.stdout
+    sys.stdout = _stringio = StringIO()
+
+    exception_raised = False
+    try:
+        func(*args, **kwds)
+    except exception:
+        exception_raised = True
+    finally:
+        sys.stdout = _stdout
+        if not exception_raised:
+            show_failure()
+            fail(str(exception) + ' not raised')
 
 def untitle(s):
     return s[0].lower() + s[1:]
